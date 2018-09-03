adb54da5d4ccd9666880280e7c81900c30e13a66
dengyt <dengyt@chinawiserv.com>
2018-09-03 15:49:19 +0800

    页面路由

---
+++ app/router/index.js
@@ -0,0 +1,50 @@
+import Vue from "vue";
+import Router from "vue-router";
+// import store from "@/store/index";
+// import { Message } from "element-ui";
+
+Vue.use(Router);
+
+const router = new Router({
+    mode: "hash",
+    routes: [{
+            path: "/",
+            redirect: "/main"
+        },
+        {
+            path: "/login",
+            name: "login",
+            component: () =>
+                import ("@/view/login/logins")
+        },
+        {
+            path: "/main",
+            name: "mainstay",
+            component: () =>
+                import ("@/view/main/mains"),
+            redirect: "/main/",
+            children: [{
+                path: "/",
+                name: "main",
+                component: () =>
+                    import ("@/view/main/index")
+            }, {
+                path: "/Refraction",
+                name: "Refraction",
+                component: () =>
+                    import ("@/view/Refraction/Refraction")
+            }]
+        }
+    ]
+});
+
+// router.beforeEach((to, from, next) => {
+//   if (to.name === "login" || store.state.user.roleId || sessionStorage.roleId) {
+//     next();
+//   } else {
+//     Message.error("未登录！");
+//     router.push("/login");
+//   }
+// });
+
+export default router;
