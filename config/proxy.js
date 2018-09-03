338a6930a1ea728b29749536ebfb1901531ce112
dengyt <dengyt@chinawiserv.com>
2018-09-03 15:53:15 +0800

    路由代理

---
+++ config/proxy.js
@@ -0,0 +1,8 @@
+module.exports = {
+    '*': {
+        target: 'http://192.168.13.142:8084',//服务器api地址
+        // target: 'http://172.16.7.52:8088',
+        changeOrigin: true,
+        secure: false
+        }
+}
