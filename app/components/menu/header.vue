bb577871b12c739af246e62ad6ba53bb4d713958
dengyt <dengyt@chinawiserv.com>
2018-09-03 15:47:11 +0800

    头部和侧边栏

---
+++ app/components/menu/header.vue
@@ -0,0 +1,74 @@
+<template>
+  <header class="header_menu">
+    <el-row class="header_sub_row">
+      <el-col :xs="9" :sm="7" :md="6" :lg="4" :xl="4">
+        <div class="headTitle">
+          <!-- <img :src='systemIcon' alt=""  class="header_icon"/> -->
+          <span class="headerTitle"><h4>NOTE</h4></span>
+        </div>
+      </el-col>
+      <el-col  :xs="15" :sm="17" :md="18" :lg="20" :xl="20">
+        <el-row>
+          <el-col :xs="22" :sm="21" :md="20" :lg="20" :xl="29" >
+            &nbsp;
+          </el-col>
+          <el-col :xs="2" :sm="3" :md="4" :lg="4" :xl="5" class="text-a-c">
+              <el-badge is-dot class="item">
+              </el-badge>
+             <img :src="userIncons" alt="" class="userInconsStyle"/>
+            <el-dropdown>
+              <span class="el-dropdown-link">
+              <span class="userTitle">{{user.userName}}<i class="el-icon-caret-bottom"></i></span>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item command="a">
+                    <i class="el-icon-message"></i>
+                    <span class="m-l-xs">消息</span>
+                    <span class="badgeStyle">{{user.emailNumber}}</span>
+                </el-dropdown-item>
+                <el-dropdown-item command="b">
+                   <!-- <img :src="exitIcon" alt='' class="userExit"/> -->
+                   <span class="userTitle" @click="exits"> 退出</span>
+                </el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </el-col>
+        </el-row> 
+      </el-col>
+    </el-row>
+  </header>
+</template>
+<script>
+  import { mapState } from 'vuex'
+  import headerIcon from '@/assets/image/u181.png'
+  import lIcon from '@/assets/image/u77.png'
+  import sysMuneDown from '@/assets/image/sysMuneIcon.png'
+  import mIcon from '@/assets/image/u66.png'
+  import userIncon from '@/assets/image/u72.png'
+  import exit from '@/assets/image/u213.png'
+
+  export default{
+    name:'headers',
+    data(){
+      return{
+        systemIcon:headerIcon,
+        systemIconMenu:sysMuneDown,
+        localIcon:lIcon,
+        emailIcon:mIcon,
+        userIncons:userIncon,
+        exitIcon: exit
+      }
+    },
+    methods:{
+      exits(){
+        sessionStorage.removeItem('roleId');
+
+        this.$router.push({name:'login'})
+      }
+    },
+    computed:{
+      ...mapState(['user'])
+    }
+  }
+  
+</script>
---
+++ app/components/menu/lefetMenu.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="left_menu">
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#25364f"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <!-- 有子菜单的菜单 -->
+      <el-submenu index="inx" v-for="(val,inx) in leftMenu" :key="inx">
+        <template slot="title">
+          <i class="el-icon-location" v-if="0"></i>
+          <span><router-link :to="val.name">{{val.title}}</router-link></span>
+        </template>
+        <el-menu-item-group v-if="val.children">
+          <!-- 子菜单 -->
+          <el-menu-item :index="inx-inxC" v-for="(valC,inxC) in val.children" :key="inxC"><router-link :to="valC.name">{{valC.title}}</router-link></el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </div>
+</template>
+<script>
+  import {mapState} from 'vuex'
+
+  export default{
+    data(){
+       return {
+      };
+    },
+    methods:{
+     
+    },
+    computed:{
+      ...mapState(['leftMenu'])
+    }
+  }
+  
+</script>
+<style scope>
+  .el-submenu__title a,.router-link-exact-active{
+    color:#fff;
+  }
+  .el-menu{
+    border: none;
+  }
+  .el-menu-item-group__title{
+      display: none;
+  }
+  .el-submenu .el-menu-item{
+    height: 30px;
+    line-height: 30px;
+  }
+</style>
